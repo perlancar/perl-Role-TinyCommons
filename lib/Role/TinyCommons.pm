9e77901cb8eb828146299337a27305cb98f27899
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2016-03-19 09:58:26 +0700

    Initial commit

---
+++ lib/Role/TinyCommons.pm
@@ -0,0 +1,17 @@
+package Role::TinyCommons;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Roles that can be commonly used, for the mutual benefit of all
+
+=head1 DESCRIPTION
+
+This module is currently empty. Use the C<Role::TinyCommons::*> directly.
+
+
+=head1 SEE ALSO
+
+Role::TinyCommons is just like L<Role::Commons>, except the roles are using
+L<Role::Tiny> instead of L<Moo::Role>>
